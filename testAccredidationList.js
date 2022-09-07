694b4f0ae7dba0b78f8b7928696b22ddbcde562e
Nikolai Maasø Lunde <aas-nmaas@ai-dev.no>
2022-09-07 12:49:20 +0200

    JWT debug

--- testAccredidationList.js
+++ testAccredidationList.js
@@ -6,6 +6,7 @@ import { getParams } from './helper.js';
 
 function accredidationTest() {
     var params = getParams(null, null);
+    console.log(params);
     group('Test accredidation list', () => {
         let res = http.get(baseUrl + '/accreditations/', params);
         assert(res,{
